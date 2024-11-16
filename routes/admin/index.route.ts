541a09cd257d66f65238f7dc246e4520b07fc1a7
unknown <phamlong123np@gmail.com>
2024-11-16 23:47:01 +0700

    Sửa giao diện bên admin

--- routes/admin/index.route.ts
+++ routes/admin/index.route.ts
@@ -8,16 +8,17 @@ import {accountRouter} from './account.route'
 import {userRouter} from './user.route'
 import {settingRouter} from './setting.route'
 import {authRouter} from './auth.route'
+import * as authMiddleware from '../../middlewares/admin/auth.middleware'
 const adminRoutes=(app:Express)=>{
     const PATH_ADMIN='/admin'
-    app.use(PATH_ADMIN+'/dashboard',dashboardRoutes)
-    app.use(PATH_ADMIN+'/songs',songRoutes)
-    app.use(PATH_ADMIN+'/roles',roleRoutes)
-    app.use(PATH_ADMIN+'/topics',topicRouter)
-    app.use(PATH_ADMIN+'/singers',singerRouter)
-    app.use(PATH_ADMIN+'/accounts',accountRouter)
-    app.use(PATH_ADMIN+'/users',userRouter)
-    app.use(PATH_ADMIN+'/settings',settingRouter)
+    app.use(PATH_ADMIN+'/dashboard',authMiddleware.requireAuth,dashboardRoutes)
+    app.use(PATH_ADMIN+'/songs',authMiddleware.requireAuth,songRoutes)
+    app.use(PATH_ADMIN+'/roles',authMiddleware.requireAuth,roleRoutes)
+    app.use(PATH_ADMIN+'/topics',authMiddleware.requireAuth,topicRouter)
+    app.use(PATH_ADMIN+'/singers',authMiddleware.requireAuth,singerRouter)
+    app.use(PATH_ADMIN+'/accounts',authMiddleware.requireAuth,accountRouter)
+    app.use(PATH_ADMIN+'/users',authMiddleware.requireAuth,userRouter)
+    app.use(PATH_ADMIN+'/settings',authMiddleware.requireAuth,settingRouter)
     app.use(PATH_ADMIN+'/auth',authRouter)
 }
 export default adminRoutes;
