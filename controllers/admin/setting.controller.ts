0e6ea96d1c8ff8f97759c2e20deb035dc91c2778
unknown <phamlong123np@gmail.com>
2024-11-16 17:21:02 +0700

    Trang setting

---
+++ controllers/admin/setting.controller.ts
@@ -0,0 +1,29 @@
+import {Request,Response} from 'express';
+import Song from '../../models/song.model';
+import * as systemConfig from '../../config/system'
+import SettingGeneral from '../../models/settings-general.model';
+export const general=async  (req:Request, res:Response) => {
+    try {
+        const settingGeneral=await SettingGeneral.findOne({})
+        res.render('admin/pages/settings/general',{
+            title:"Cài đặt chung",
+            settingGeneral:settingGeneral
+        })
+    } catch (error) {
+        
+    }
+    
+}
+export const generalPatch=async  (req:Request, res:Response) => {
+    const settingGeneral=await SettingGeneral.findOne({});
+    if(settingGeneral){
+        await SettingGeneral.updateOne({
+            _id:settingGeneral.id
+        },req.body)
+    }
+    else{
+        const settingGeneral=new SettingGeneral(req.body);
+        await settingGeneral.save()
+    }
+    res.redirect('back')
+}
--- routes/admin/index.route.ts
+++ routes/admin/index.route.ts
@@ -6,6 +6,7 @@ import {topicRouter} from './topic.route'
 import {singerRouter} from './singer.route'
 import {accountRouter} from './account.route'
 import {userRouter} from './user.route'
+import {settingRouter} from './setting.route'
 const adminRoutes=(app:Express)=>{
     const PATH_ADMIN='/admin'
     app.use(PATH_ADMIN+'/dashboard',dashboardRoutes)
@@ -15,5 +16,7 @@ const adminRoutes=(app:Express)=>{
     app.use(PATH_ADMIN+'/singers',singerRouter)
     app.use(PATH_ADMIN+'/accounts',accountRouter)
     app.use(PATH_ADMIN+'/users',userRouter)
+    app.use(PATH_ADMIN+'/settings',settingRouter)
+    app.use(PATH_ADMIN+'/auth',settingRouter)
 }
 export default adminRoutes;
