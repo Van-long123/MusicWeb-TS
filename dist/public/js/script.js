d534619b4ab3765387250a65d287f971da41e6d4
unknown <phamlong123np@gmail.com>
2025-01-01 17:26:46 +0700

    Thêm phần bannar ở trang chủ

--- dist/public/js/script.js
+++ dist/public/js/script.js
@@ -207,3 +207,19 @@ if(buttonPagination.length > 0){
     })
 }
 // pagination
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
